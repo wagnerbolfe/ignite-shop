7f8d8134d5bb2d756bfda761e39d022ebe733e5e
Wagner Bolfe <shaper@live.com>
2023-07-30 22:39:59 -0300

    Initial Commit

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
@@ -10,9 +9,8 @@ export const HomeContainer = styled('main', {
 })
 
 export const Product = styled('a', {
-  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  background: 'linear-gradient(120deg, #4eabf8 0%, #ff5f42 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
@@ -22,7 +20,9 @@ export const Product = styled('a', {
   justifyContent: 'center',
 
   img: {
-    objectFit: 'cover'
+    objectFit: 'cover',
+    width: 'auto',
+    height: 'auto'
   },
 
   footer: {
